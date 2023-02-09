135927d1eb9b99db09428cb3679c22a456579490
icode4living <samauelsamafolabi@outlook.com>
2023-02-09 08:30:52 +0100

    status code implementation in progress

---
+++ src/utils/status.rs
@@ -0,0 +1,75 @@
+/**HTTP status code */
+#[derive(Debug)]
+pub enum Informational{
+    Continue,
+    SwitchingProtocols,
+    Processing,
+}
+#[derive(Debug)]
+pub enum Success{
+    Ok,
+    Created,
+    Accepted,
+    NonAuthoritativeInformation,
+    NoContent,
+    ResetContent,
+    PartialContent,
+    MultiStatus,
+    AlreadyReported,
+    ImUsed,
+}
+#[derive(Debug)]
+pub enum Redirection{
+    MultipleChoices,
+    MovedPermanently,
+    Found,
+    SeeOther,
+    NotModified,
+    UseProxy,
+    TemporaryRedirect,
+    PermanentRedirect,
+}
+#[derive(Debug)]
+pub enum ClientError{
+    BadRequest,
+    Unauthorized,
+    PaymentRequired,
+    Forbidden,
+    NotFound,
+    MethodNotAllowed,
+    NotAcceptable,
+    ProxyAuthenticationRequired,
+    RequestTimeout,
+    Conflict,
+    Gone,
+    LengthRequired,
+    PreconditionFailed,
+    PayloadTooLarge,
+    RequestUriTooLong,
+    UnsupportedMediaType,
+    RequestedRangeNotSatisfiable,
+    ExpectationFailed,
+    MisdirectedRequest,
+    UnprocessableEntity,
+    Locked,
+    FailedDependency,
+    UpgradeRequired,
+    PreconditionRequired,
+    TooManyRequests,
+    RequestHeaderFieldsTooLarge,
+    ConnectionClosedWithoutResponse,
+    ClientClosedRequest,
+}
+pub enum ServerError{
+    InternalServerError,
+    NotImplemented,
+    BadGateway,
+    ServiceUnavailable,
+    GatewayTimeout,
+    HttpVersionNotSupported,
+    VariantAlsoNegotiates,
+    InsufficientStorage,
+    LoopDetected,
+    NetworkAuthenticationRequired,
+    NetworkConnectTimeoutError,
+}
