f14f950f11e7c52e2a41029cca495214b4f6642e
samuelsamafolabi@outlook.com <samuelsamafolabi@outlook.com>
2023-02-16 16:06:22 +0100

    http parser skeleton code

--- src/utils/http_parser.rs
+++ src/utils/http_parser.rs
@@ -1,3 +1,22 @@
 /*HTTP header parser checks if the request header matches the 
 http specification
  */
+use std::fs;
+extern crate pest;
+//#[macro_use]
+use pest_derive::Parser;
+
+use pest::Parser;
+#[derive(Parser)]
+#[grammar = "grammars/http.pest"]
+pub struct HTTPParser;
+
+//skeleton test file
+pub fn htpp_parser(){
+    let unpassed_file = fs::read_to_string("sample/test_file.http")
+    .expect("file could not be opened");
+
+let unsuccessful_parse = HTTPParser::parse(Rule::uri, &unpassed_file)
+.expect("unsucceful parse").next().unwrap();
+println!("{:?}", unsuccessful_parse);
+}
---
+++ tests/http_parse_test.rs
@@ -0,0 +1,5 @@
+use flashweb::utils::http_parser::*;
+#[test]
+pub fn test_http(){
+    htpp_parser();
+}
