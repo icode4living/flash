75dce0a176669b30bbb3d974f96383d91537905c
samuelsamafolabi@outlook.com <samuelsamafolabi@outlook.com>
2023-02-17 17:30:11 +0100

    HTTP parser issue

--- src/utils/http_parser.rs
+++ src/utils/http_parser.rs
@@ -1,7 +1,7 @@
 /*HTTP header parser checks if the request header matches the 
 http specification
  */
-use std::fs;
+use std::{fs, collections::{HashMap, hash_map::RandomState}, hash::Hash};
 extern crate pest;
 //#[macro_use]
 use pest_derive::Parser;
@@ -11,12 +11,15 @@ use pest::Parser;
 #[grammar = "grammars/http.pest"]
 pub struct HTTPParser;
 
-//skeleton test file
-pub fn htpp_parser(){
-    let unpassed_file = fs::read_to_string("sample/test_file.http")
-    .expect("file could not be opened");
 
-let unsuccessful_parse = HTTPParser::parse(Rule::uri, &unpassed_file)
-.expect("unsucceful parse").next().unwrap();
-println!("{:?}", unsuccessful_parse);
+//Hash map for storing parsed http header value
+pub fn parse_http<T> (item:String)-> HashMap<T, HashMap<T,T>>
+where T: Eq,
+    T: Hash,
+    T: ToString   
+{
+    let mut header: HashMap<T, HashMap<T,T>> = HashMap::new();
+    header.insert("sam", "Afo");
+   // header.insert(&"name", &"sam");
+    header
 }
--- tests/http_parse_test.rs
+++
@@ -1,5 +0,0 @@
-use flashweb::utils::http_parser::*;
-#[test]
-pub fn test_http(){
-    htpp_parser();
-}
