264191cf922fc1f7e9cee9714581266e60fc51c3
icode4living <samauelsamafolabi@outlook.com>
2023-02-20 08:10:25 +0100

    http parser issue fixed

--- src/utils/http_parser.rs
+++ src/utils/http_parser.rs
@@ -7,17 +7,78 @@ extern crate pest;
 use pest_derive::Parser;
 
 use pest::Parser;
+//use pest::iterators::Pair;
 #[derive(Parser)]
 #[grammar = "grammars/http.pest"]
 pub struct HTTPParser;
 
 
-pub fn store_http(input: &'static str)->HashMap<&'static str, HashMap<&'static str,&'static str>> {
-    let mut my_value:HashMap<&str, HashMap<&str,&str>>= HashMap::new();
-  //  my_value.insert("name", input);
-    
+pub fn store_http(item: String)->HashMap<String,HashMap<String,String>>{
+  let mut my_value:HashMap<String,HashMap<String,String>> = HashMap::new();
 
+ // my_value.insert("test", "test");
+   //let mut req = "";
+
+    let item = HTTPParser::parse(Rule::http, &item)
+    .expect("unsuccessful parse")
+    .next().unwrap();
+    for line in item.into_inner(){
+      match line.as_rule(){
+        Rule::request=>{
+          let mut inner_rules = line.into_inner();
+        //  req = inner_rules.next().unwrap().as_str();
+          let nvalue = my_value.entry("request".to_owned()).or_default();
+         nvalue.insert("method".to_owned(), inner_rules.next().unwrap().as_str().to_owned());
+          nvalue.insert("path".to_owned(), inner_rules.next().unwrap().as_str().to_owned());
+          nvalue.insert("version".to_owned(), inner_rules.next().unwrap().as_str().to_owned());
+          nvalue.insert("cookie".to_owned(), inner_rules.next().unwrap().as_str().to_owned());
+
+
+         //my_value.insert("method".to_owned(), method.to_owned());
+
+        }
+        Rule::request_line=>{
+        
+        }
+        Rule::uri=>{
+
+        }
+        Rule::method=>{
+
+        }
+        Rule::version =>{
+
+        }
+        Rule::whitespace=>{
+
+        }
+        Rule::headers=>{
+
+        }
+        Rule::header=>{
+
+        }
+        Rule::header_name=>{
+
+        }
+        Rule::header_value=>{
+
+        }
+        Rule::delimiter=>{
+
+        }
+        Rule::http=>{
+
+        }
+
+        Rule::EOI=>()
+      }
+      
+    }
+     // my_value
+   //println!("{:?}", my_value)
    my_value
+
 }
 
 
--- tests/http_parse_test.rs
+++ tests/http_parse_test.rs
@@ -1,9 +1,12 @@
 use flashweb::utils::http_parser::*;
+use std::fs;
 
-/*#[test]
+#[test]
 pub fn test_http(){
-let p= store_http("sam");
-println!("{}", p.get("name").unwrap())
+    let file = fs::read_to_string("sample/test_file.http")
+    .expect("file could not be read");
+    let f = store_http(file);
+    println!("{}",f["request"]["cookie"])
+
 }
-*/
 
